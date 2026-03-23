a9aac8b195f8f9c767566c58f81601261dd868ad
LIBERATO MAYK <liberato13mayk@gmail.com>
2026-03-23 00:05:49 -0600

    eliminando las carpetas duplicadas

--- models/m_admin.py
+++
@@ -1,9 +0,0 @@
-from sqlmodel import SQLModel, Field
-from typing import Optional
-
-class Admin(SQLModel, table=True):
-    __tablename__ = "admins"
-
-    id: Optional[int] = Field(default=None, primary_key=True)
-    username: str = Field(index=True, unique=True)
-    hashed_password: str
