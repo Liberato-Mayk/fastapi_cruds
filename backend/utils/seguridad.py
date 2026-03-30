5488869969f23a12b132d5802d6c510a96eb0f72
LIBERATO MAYK <liberato13mayk@gmail.com>
2026-03-29 23:16:55 -0600

    areglando la seguridad

--- backend/utils/seguridad.py
+++ backend/utils/seguridad.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-from jose import JWTError, jwt
+from jose import jwt
 from passlib.context import CryptContext
 
 SECRET_KEY = "SUPER_SECRET_KEY"
@@ -17,12 +17,6 @@ def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
 # Crear JWT
-def get_password_hash(password: str):
-    return pwd_context.hash(password)
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
 def create_access_token(data: dict):
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
